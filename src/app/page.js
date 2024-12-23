ebf397f3ab2815720580dc623018ca05ee69dc9c
Antoine <antoine.richard-capponi@epita.fr>
2024-12-23 01:13:19 +0100

    📚 Added 3D Scene Proof of Concept

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Spline from "@splinetool/react-spline";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <iframe src="https://horizonscene.vercel.app"></iframe>
         <ol>
           <li>
-            Welcome to <code>HorizonWeb</code>.
+            🌇 Bienvenue sur <code>HorizonWeb</code> [beta].
           </li>
-          <li>Coming Soon.</li>
+          <li>
+            <em>👀 Psst Scrolle dans l&apos;iFrame </em>
+          </li>
         </ol>
       </main>
     </div>
