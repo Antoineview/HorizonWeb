ff0ab3654925fe4e6b67563a8470d66553db805e
Louis CHABERT <95475400+louischabert@users.noreply.github.com>
2025-01-09 17:25:51 +0100

    Suppression de certains émojis dans la section "notre équipe"

--- src/app/Sections/equipe.js
+++ src/app/Sections/equipe.js
@@ -8,7 +8,7 @@ import styles from "../Styles/equipe.module.css";
 const teamMembers = [
   {
     id: 1,
-    name: "Fanny ALACOQUE ✨",
+    name: "Fanny ALACOQUE",
     role: "Responsable du graphisme 🎨 et du mode multijoueur 🎮",
     description:
       "Née en 2006, Fanny est la cheffe de projet et l'image de l'équipe. Passionnée d'informatique et experte en programmation, elle insuffle créativité et innovation au projet. Grande amatrice de jeux vidéo, elle met sa connaissance approfondie du domaine au service de l'équipe, faisant d'elle une source d'inspiration incontournable.",
@@ -17,22 +17,22 @@ const teamMembers = [
   {
     id: 2,
     name: "Antoine RICHARD-CAPPONI 🖌️",
-    role: "Responsable de la R&D des niveaux 🗺️",
+    role: "Responsable de la R&D des niveaux",
     description:
       "Antoine, né en 2007, est sous-chef de l'équipe et gère la planification des réunions. Créatif et passionné, il conçoit des designs uniques et explore des idées de gameplay innovantes, inspirées notamment par l'origami. Admirateur du design épuré d'Apple, il apporte une expertise technique précieuse à l'équipe, notamment, sur le site web.",
     image: "/antoine.png",
   },
   {
     id: 3,
-    name: "Louis CHABERT 📚",
-    role: "Responsable de la création 🖥️ et de la gestion du site web 🌐",
+    name: "Louis CHABERT",
+    role: "Responsable de la création et de la gestion du site web 🌐",
     description:
       "Né en 2006, Louis est un pilier de l'équipe, apprécié pour sa rigueur et son sens de l'organisation. Son expérience en assistance technique chez Apple enrichit le projet de sa patience et de son altruisme. Amateur de voyages, il puise son inspiration dans les paysages tropicaux et la plongée sous-marine.",
     image: "/louis.png",
   },
   {
     id: 4,
-    name: "Garance DUFRAISSE 🌸",
+    name: "Garance DUFRAISSE",
     role: "Responsable de l'intelligence artificielle 🤖",
     description:
       "Née en 2006, Garance est une touche-à-tout curieuse et polyvalente, avec un excellent œil pour les détails. Forte de cinq années passées au Japon, elle combine ouverture d'esprit et créativité. Son expertise en orthographe et son enthousiasme pour les projets insolites en font une alliée précieuse pour l'équipe.",
@@ -40,7 +40,7 @@ const teamMembers = [
   },
   {
     id: 5,
-    name: "El-Ferdaous BOUHARIS 🌱",
+    name: "El-Ferdaous BOUHARIS",
     role: "Responsable du son 🎵 et de l'animation 🎥",
     description:
       "El-Ferdaous, née en 2006, est passionnée par l'écologie et la biodiversité. Son talent pour concevoir des paysages naturels réalistes, associé à sa créativité et son aisance à communiquer, fait d'elle un atout clé de l'équipe. Sa vision durable enrichit le projet à chaque étape.",
