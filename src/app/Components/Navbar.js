d3d4fa30e647f876489b4a003818a70e7a6ca068
Louis CHABERT <95475400+louischabert@users.noreply.github.com>
2025-01-11 10:53:28 +0100

    Optimisation navbar pour mobiles

--- src/app/Components/Navbar.js
+++ src/app/Components/Navbar.js
@@ -1,49 +1,50 @@
 "use client";
-
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import styles from "../Styles/navbar.module.css";
 
 export default function Navbar() {
   const [isVisible, setIsVisible] = useState(true);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const handleScroll = () => {
-    // Show navbar only at the top of the page
     setIsVisible(window.scrollY <= 15);
+    setIsMobileMenuOpen(false);
   };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <motion.div
+    <motion.nav
       className={styles.navbar}
       initial={{ y: 0 }}
-      animate={{ y: isVisible ? 0 : -100 }} // Slide navbar out of view
+      animate={{ y: isVisible ? 0 : -100 }}
       transition={{ duration: 0.3, ease: "easeInOut" }}
     >
       <div className={styles.navContent}>
         <h1 className={styles.logo}>HorizonWeb</h1>
-        <ul className={styles.navLinks}>
-          <li>
-            <a href="#entreprise">Notre entreprise</a>
-          </li>
-          <li>
-            <a href="#timeline">Feuille de route</a>
-          </li>
-          <li>
-            <a href="#equipe">Notre équipe</a>
-          </li>
-          <li>
-            <a href="#jeu">Notre jeu</a>
-          </li>
-        </ul>
+        
+        <button 
+          className={styles.hamburger}
+          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+        >
+          ☰
+        </button>
+
+        <div className={`${styles.menuContainer} ${isMobileMenuOpen ? styles.active : ''}`}>
+          <ul className={styles.navLinks}>
+            <li><a href="#entreprise">Notre entreprise</a></li>
+            <li><a href="#timeline">Feuille de route</a></li>
+            <li><a href="#equipe">Notre équipe</a></li>
+            <li><a href="#jeu">Notre jeu</a></li>
+          </ul>
+        </div>
       </div>
-    </motion.div>
+    </motion.nav>
   );
 }
